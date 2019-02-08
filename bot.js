b1886d9148830bf18c640f16c286a04949d06f98
Omarpro1 <47451637+Omarpro1@users.noreply.github.com>
2019-02-08 15:50:58 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("542699575086153729")
+setInterval(function() {
+channel.send(`alone in my life :D`);
+}, 30)
+})
+
+client.login(NTQzNDE0Nzk3MzUwMjA3NDk1.Dz8OOQ.rFf1vGZc6v7VvvvLiP7HOEDNexo);
